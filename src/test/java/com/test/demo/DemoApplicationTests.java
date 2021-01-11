36b5da7e4289c572b60e1bcfa761c5b1f38a0028
ediantoeddie <77226549+ediantoeddie@users.noreply.github.com>
2021-01-11 22:20:12 +0700

    add sample unit test

--- src/test/java/com/test/demo/DemoApplicationTests.java
+++ src/test/java/com/test/demo/DemoApplicationTests.java
@@ -1,13 +1,46 @@
 package com.test.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.test.demo.controller.DemoController;
+import com.test.demo.entity.Demo;
+
+
+//@ExtendWith(SpringExtension.class)
+//@WebMvcTest(DemoController.class)
 @SpringBootTest
 class DemoApplicationTests {
 
+//	@Autowired
+//	private MockMvc mvc;
+//	
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	void getAll() throws Exception {
+//		RequestBuilder request = MockMvcRequestBuilders.get("get/all");
+//		MvcResult result = mvc.perform(request).andReturn();
+		assertEquals(2, 2);
+	}
 
+	void add() throws Exception {
+//		RequestBuilder request = MockMvcRequestBuilders.get("get/all");
+//		MvcResult result = mvc.perform(request).andReturn();
+		assertEquals(2, 2);
+	}
 }
